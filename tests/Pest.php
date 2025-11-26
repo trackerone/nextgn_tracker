534f9e7fcc4f0372d26b1fd5810a41038c2452a9
trackerone <51250404+trackerone@users.noreply.github.com>
2025-11-27 00:26:12 +0100

    Update Pest.php

--- tests/Pest.php
+++ tests/Pest.php
@@ -5,9 +5,10 @@ declare(strict_types=1);
 use Database\Seeders\RoleSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Schema;
+use Tests\TestCase;
 
-uses(Tests\TestCase::class)->in('Feature', 'Unit');
-uses(RefreshDatabase::class)->in('Feature');
+uses(TestCase::class)->in('Feature', 'Unit');
+uses(RefreshDatabase::class)->in('Feature', 'Unit');
 
 beforeEach(function (): void {
     if (Schema::hasTable('roles')) {
