160d0b6d2ac249c5f02cce8b1138d4748dda768a
trackerone <51250404+trackerone@users.noreply.github.com>
2025-12-28 12:54:49 +0000

    q

--- storage/framework/views/acc51de9409ad081d4601da7e004aa89.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('title', __('Page not found')); ?>
-<?php $__env->startSection('status', '404'); ?>
-<?php $__env->startSection('message', __('The page you are looking for could not be found.')); ?>
-
-<?php echo $__env->make('errors.layout', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH /workspaces/nextgn_tracker/resources/views/errors/404.blade.php ENDPATH**/ ?>
