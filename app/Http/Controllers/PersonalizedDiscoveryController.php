6603c811924a709ee1b1612a6f297ad592350257
trackerone <51250404+trackerone@users.noreply.github.com>
2026-06-05 13:35:28 +0200

    feat: add discovery dashboard ux

--- app/Http/Controllers/PersonalizedDiscoveryController.php
+++ app/Http/Controllers/PersonalizedDiscoveryController.php
@@ -4,14 +4,22 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Services\Torrents\DiscoveryDashboardBuilder;
 use App\Services\Torrents\PersonalizedDiscoveryFeedBuilder;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 final class PersonalizedDiscoveryController extends Controller
 {
-    public function __invoke(Request $request, PersonalizedDiscoveryFeedBuilder $feedBuilder): View
+    public function __invoke(
+        Request $request,
+        DiscoveryDashboardBuilder $dashboardBuilder,
+        PersonalizedDiscoveryFeedBuilder $feedBuilder
+    ): View
     {
-        return view('account.discovery', $feedBuilder->buildFor($request->user()));
+        return view('account.discovery', array_merge(
+            $dashboardBuilder->build(),
+            $feedBuilder->buildFor($request->user())
+        ));
     }
 }
